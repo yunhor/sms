51303c5461dea290237e647c3a34e6dc6f199529
yunhor <yunhor@163.com>
2018-03-15 18:25:06 +0800

    add testsend

--- _examples/sendtest.go
+++ _examples/sendtest.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/BurntSushi/toml"
+	"github.com/yunhor/alisms"
 )
 
 func main() {
-	var Cfg string
-	if _, err := toml.DecodeFile("sms.toml", &Cfg); err != nil {
+	var cfg alisms.UserParams
+	if _, err := toml.DecodeFile("sms.toml", &cfg); err != nil {
 		return
 	}
+	fmt.Println(cfg.AccessKeyId)
+	fmt.Println(cfg.AppSecret)
+	cfg.TemplateParam = fmt.Sprintf("{\"name\":\"%s\",\"money\":\"%s\",\"time\":\"%s\"}", "中文名字", "8000", "1月20至2月22日")
+	rt, str, err := alisms.SendMessage(&cfg)
+	fmt.Println(rt)
+	fmt.Println(str)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
 }
